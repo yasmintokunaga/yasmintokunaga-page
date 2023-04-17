2c77f3f7593c73524be7a594b179d898418d3b67
yasmintokunaga <yasmintkng@gmail.com>
2023-04-17 19:26:08 -0300

    config path name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return(
       <Switch>
-        <Route exact path="/" component={ Home } />
+        <Route exact path="/yasmintokunaga-page" component={ Home } />
         <Route path="/expercienceeducation" component={ ExperienceEducation } />
         <Route path="/skills" component={ Skills } />
         <Route path="/portfolio" component={ Portfolio } />
